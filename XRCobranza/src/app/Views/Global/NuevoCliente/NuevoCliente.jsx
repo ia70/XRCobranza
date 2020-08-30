3034a6f3ad4090c6c100fc5b2bfe4e6c5f028c0d
xcoru <pcd510@hotmail.com>
2020-08-30 16:05:52 -0500

    Update NuevoCliente.jsx

--- XRCobranza/src/app/Views/Global/NuevoCliente/NuevoCliente.jsx
+++ XRCobranza/src/app/Views/Global/NuevoCliente/NuevoCliente.jsx
@@ -13,17 +13,20 @@ import keys from '../../../../keys';
 import { getDateTime } from '../../../../lib/util';
 
 import Logo from '../../../img/Logo.png';
+import { MultiCompiler } from 'webpack';
 
 class NuevoCliente extends Component {
 
     constructor(props) {
         super(props);
 
+        this._cargando = false;
         sessionStorage.setItem('route', 'nuevocliente');
 
         this.state = {
             login: sessionStorage.getItem('login'),
             user: sessionStorage.getItem('user'),
+            id_user: sessionStorage.getItem('id_user'),
             sucursal: sessionStorage.getItem('sucursal'),
             hash: sessionStorage.getItem('hash'),
             rol: sessionStorage.getItem('rol'),
@@ -42,10 +45,11 @@ class NuevoCliente extends Component {
                 this.setState({ filtro: [] });
             } else {
                 if (this._isMounted == true) {
-                    var url ="http://" + keys.database.host +":"+ keys.server.port + keys.api.url + 'persona/filtrar_clientes';
+                    var url = "http://" + keys.database.host + ":" + keys.server.port + keys.api.url + 'persona/filtrar_clientes';
 
                     var data_text = {
                         user: this.state.user,
+                        id_user: this.state.id_user,
                         sucursal: this.state.sucursal,
                         hash: this.state.hash,
                         filtro: cadena
@@ -86,103 +90,102 @@ class NuevoCliente extends Component {
 
     enviar() {
         let ruta = "";
-        let _ine="";
+        let _ine = "";
         let _long_ine = 0;
         try {
-            ruta = document.getElementById('c_ruta').value;
-            _ine = document.getElementById('p_ine').value;
-            _long_ine = _ine.length;
-            console.log(_long_ine);
+            if (this._cargando != true) {
+                this._cargando = true;
+                ruta = document.getElementById('p_id_ruta').value;
+                _ine = document.getElementById('p_ine').value;
+                _long_ine = _ine.length;
+                console.log(_long_ine);
 
-            if (_long_ine != 13 && _long_ine != 10) {
-                alert("¡Clave INE erronea!");
-            } else
-                if (ruta != null) {
-                    if (ruta != "0") {
-                        var url = "http://" + keys.database.host + keys.api.url + 'nuevo_cliente';
+                if (_long_ine != 13 && _long_ine != 10) {
+                    alert("¡Clave INE erronea!");
+                } else
+                    if (ruta != null) {
+                        if (ruta != "0") {
+                            var url = "http://" + keys.database.host + keys.api.url + 'nuevo_cliente';
 
-                        var data_text = {
-                            persona: {
-                                ine: document.getElementById('p_ine').value,
-                                ine_ref: null,
-                                alias: document.getElementById('p_alias').value,
-                                nombre: document.getElementById('p_nombre').value,
-                                apellido_paterno: document.getElementById('p_apaterno').value,
-                                apellido_materno: document.getElementById('p_amaterno').value,
-                                id_calle: 1,
-                                direccion: document.getElementById('p_direccion').value,
-                                no_casa: document.getElementById('p_no_casa').value,
-                                referencias: document.getElementById('p_referencia').value,
-                                foto_casa: null,
-                                telefono: document.getElementById('p_telefono').value,
-                                correo: document.getElementById('p_email').value,
-                                foto: null,
-                                ine_img: null,
-                                id_tipo_inmueble: document.getElementById('p_tipo_inmueble').value,
-                                id_estado: 5,
-                                fecha_reg: getDateTime()
-                            },
-                            establecimiento: {
-                                ine: document.getElementById('p_ine').value,
-                                nombre: document.getElementById('n_nombre').value,
-                                id_calle: null,
-                                numero_ext: null,
-                                referencias: document.getElementById('n_referencia').value,
-                                id_actividad_economica: null,
-                                id_tipo_inmueble: document.getElementById('n_tipo_inmueble').value,
-                                id_tipo_local: document.getElementById('n_tipo_local').value,
-                                documento_adicional: null,
-                                disponibilidad_hr_inicio: document.getElementById('n_hora_inicio').value,
-                                disponibilidad_hr_fin: document.getElementById('n_hora_fin').value,
-                                coordenadas: document.getElementById('n_coordenadas').value,
-                                foto: null,
-                                id_ruta: document.getElementById('c_ruta').value,
-                                id_estado: 5
-                            },
-                            aval: {
-                                ine: document.getElementById('p_ine').value,
-                                nombre: document.getElementById('a_nombre').value,
-                                apellido_paterno: document.getElementById('a_apaterno').value,
-                                apellido_materno: document.getElementById('a_amaterno').value,
-                                direccion: null,
-                                telefono: document.getElementById('a_telefono').value,
-                                parentesco: document.getElementById('a_parentesco').value
-                            },
-                            user: {
-                                user: this.state.user,
-                                sucursal: this.state.sucursal,
-                                hash: this.state.hash,
-                                rol: this.state.rol
-                            }
-                        };
-
-                        fetch(url, {
-                            method: 'POST',
-                            body: JSON.stringify(data_text),
-                            headers: {
-                                'Content-Type': 'application/json'
-                            }
-                        }).then(res => res.json())
-                            .catch(error => {
-                                console.error('Error:', error)
-                            })
-                            .then(response => {
-                                if (response.session) {
-                                    if (response.response) {
-                                        alert('¡Registro guardado!');
-                                    } else
-                                        alert('¡Error al insertar!');
-                                } else {
-                                    sessionStorage.clear();
-                                    alert('¡Sesion bloqueada!');
-                                    this.setState({ login: false });
+                            var data_text = {
+                                persona: {
+                                    ine: document.getElementById('p_ine').value,
+                                    alias: document.getElementById('p_alias').value,
+                                    nombre: document.getElementById('p_nombre').value,
+                                    apellido_paterno: document.getElementById('p_apaterno').value,
+                                    apellido_materno: document.getElementById('p_amaterno').value,
+                                    direccion: document.getElementById('p_direccion').value,
+                                    no_casa: document.getElementById('p_no_casa').value,
+                                    referencias: document.getElementById('p_referencia').value,
+                                    telefono: document.getElementById('p_telefono').value,
+                                    id_tipo_inmueble: document.getElementById('p_tipo_inmueble').value,
+                                    disp_hr_ini: document.getElementById('p_disp_hr_ini').value,
+                                    disp_hr_fin: document.getElementById('p_disp_hr_fin').value,
+                                    coordenadas: document.getElementById('p_coordenadas').value,
+                                    clave_media: null,
+                                    id_estado: 1,
+                                    fecha_reg: getDateTime()
+                                },
+                                usuario: {
+                                    usuario: document.getElementById('p_ine').value,
+                                    password: "12345",
+                                    id_persona: null,
+                                    usuario_padre: this.state.id_user,
+                                    id_rol: 3,
+                                    id_ruta: document.getElementById('p_id_ruta').value,
+                                    id_empresa: 1,
+                                    id_sucursal: 1,
+                                    id_estado: 1,
+                                    fecha_reg: getDateTime()
+                                },
+                                aval: {
+                                    ine: document.getElementById('p_ine').value,
+                                    nombre: document.getElementById('a_nombre').value,
+                                    apellido_paterno: document.getElementById('a_apaterno').value,
+                                    apellido_materno: document.getElementById('a_amaterno').value,
+                                    direccion: document.getElementById('a_direccion').value,
+                                    telefono: document.getElementById('a_telefono').value,
+                                    id_tipo_parentesco: document.getElementById('a_id_tipo_parentesco').value
+                                },
+                                user: {
+                                    user: this.state.user,
+                                    id_user: this.state.id_user,
+                                    sucursal: this.state.sucursal,
+                                    hash: this.state.hash,
+                                    rol: this.state.rol
                                 }
-                            });
-                    } else {
-                        alert("¡Seleccione la ruta!");
+                            };
+
+                            fetch(url, {
+                                method: 'POST',
+                                body: JSON.stringify(data_text),
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            }).then(res => res.json())
+                                .catch(error => {
+                                    console.error('Error:', error)
+                                })
+                                .then(response => {
+                                    if (response.session) {
+                                        if (response.response) {
+                                            alert('¡Registro guardado!');
+                                        } else
+                                            alert('¡Error al insertar!');
+                                    } else {
+                                        sessionStorage.clear();
+                                        alert('¡Sesion bloqueada!');
+                                        this.setState({ login: false });
+                                    }
+                                });
+                        } else {
+                            alert("¡Seleccione la ruta!");
+                        }
                     }
-                }
+                    this._cargando = false;
+            }
         } catch (error) {
+            this._cargando = false;
             console.log(error);
         }
     }
@@ -211,7 +214,7 @@ class NuevoCliente extends Component {
                 <Navbar setLogo={Logo} title="Nuevo Cliente" setButton={true} />
                 <div className="container-fluid">
                     <div className="row" >
-                        
+
                         <Title title="DATOS PERSONALES" />
 
                         <TextBox id="p_ine" label="INE" holder="Clave de Credencial" help="" required={true} maxlength={20} />
@@ -240,16 +243,16 @@ class NuevoCliente extends Component {
                         <ComboBox id="a_listaclientes" label="Cliente" items={this.state.filtro} value={"ine"} description={"nombre"} evento={this.leer} />
 
                         <TextBox id="a_nombre" label="Nombre" holder="Nombre del cliente" help="" required={true} maxlength={50} />
-                        <TextBox id="a_apaterno" label="A. Paterno" holder="Apellido paterno" help="" required={true} maxlength={20} />
-                        <TextBox id="a_amaterno" label="A. Materno" holder="Apellido materno" help="" required={true} maxlength={20} />
+                        <TextBox id="a_apellido_paterno" label="A. Paterno" holder="Apellido paterno" help="" required={true} maxlength={20} />
+                        <TextBox id="a_apellido_materno" label="A. Materno" holder="Apellido materno" help="" required={true} maxlength={20} />
                         <TextBox id="a_direccion" label="Dirección" holder="Dirección" help="" required={true} maxlength={250} />
                         <TextPhoneNumber id="a_telefono" label="Telefono" holder="Telefono" help="" required={false} />
 
-                        <ComboBox id="a_parentesco" label="Parentesco" tabla='tipo_parentesco' value={"id_tipo_parentesco"} description={"descripcion"} ></ComboBox>
-            
+                        <ComboBox id="a_id_tipo_parentesco" label="Parentesco" tabla='tipo_parentesco' value={"id_tipo_parentesco"} description={"descripcion"} ></ComboBox>
+
 
                         <Title title="COBRANZA" />
-                        <ComboBox id="c_ruta" label="Asignar a ruta" tabla='ruta' value={"id_ruta"} description={"descripcion"} ></ComboBox>
+                        <ComboBox id="p_id_ruta" label="Asignar a ruta" tabla='ruta' value={"id_ruta"} description={"descripcion"} ></ComboBox>
 
                     </div>
                     <div className="row">
