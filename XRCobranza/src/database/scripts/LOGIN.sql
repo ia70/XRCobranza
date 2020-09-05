02cbe00822171023f7cc0a5dcc39d0ec412ea165
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-09-05 13:55:29 -0500

    Update LOGIN.sql

--- XRCobranza/src/database/scripts/LOGIN.sql
+++ XRCobranza/src/database/scripts/LOGIN.sql
@@ -1,9 +1,24 @@
-DROP PROCEDURE IF EXISTS LISTA_SOLICITUDES;
+DROP PROCEDURE IF EXISTS LOGIN;
  DELIMITER //
-CREATE PROCEDURE LISTA_SOLICITUDES(IN usuario VARCHAR(100))
+CREATE PROCEDURE LOGIN(IN usuario VARCHAR(50), IN pass VARCHAR(100))
 	BEGIN
-		SELECT a.ine, a.alias, CONCAT_WS(' ', a.nombre, a.apellido_paterno, a.apellido_materno) AS "nombre", b.monto, b.pagos, b.id_estado_solicitud 
-		FROM persona a INNER JOIN solicitud AS b ON b.ine = a.ine
-		WHERE b.id_usuario = usuario;
+		SELECT 
+			u.id_usuario, 
+			CONCAT_WS(" ", p.nombre, p.apellido_paterno) AS "nombre", 
+			u.id_rol, 
+			u.id_ruta, 
+			u.id_empresa, 
+			u.id_sucursal, 
+			c.id_moneda, 
+			c.id_zona_horaria
+		FROM usuario u
+		LEFT JOIN persona AS p ON u.id_persona = p.id_persona
+		LEFT JOIN empresa AS e ON e.id_empresa = u.id_empresa
+		LEFT JOIN sucursal AS s ON s.id_sucursal = u.id_sucursal
+		LEFT JOIN configuracion AS c ON u.id_empresa = c.id_empresa
+
+		WHERE u.usuario = usuario AND u.password = pass 
+		AND (u.id_estado = 1 AND p.id_estado = 1 AND e.id_estado = 1 
+		AND (s.id_estado = 1 OR u.id_rol = 1) AND c.licencia_fin >= CURDATE() OR u.id_rol = 4);
 	END //
 DELIMITER ;
