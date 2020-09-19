326e1fbaf5114623b74086aa8bcb329693838028
xcoru <pcd510@hotmail.com>
2020-09-19 05:45:33 -0500

    Update database.js

--- XRCobranza/src/lib/database.js
+++ XRCobranza/src/lib/database.js
@@ -10,19 +10,19 @@ const pool = mysql.createPool(database);
 pool.getConnection((err, connection) => {
     if (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
+            console.error('DATABASE CONNECTION WAS CLOSED!');
         }
         if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has to many connections');
+            console.error('DATABASE HAS TO MANY CONNECTIONS!');
         }
         if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused');
+            console.error('NOT EXIST DATABASE CONNECTION!');
         }
     }
 
     if (connection) {
         connection.release();
-        console.log('DB is Connected');
+        console.log('DATABASE IS CONNECTED!');
     }
 
 
