f420ea8d821e1a1dfa5e5a25242a12732b60ba3c
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-09-26 06:20:47 -0500

    Update session_verifier.js

--- XRCobranza/src/lib/Guard/session_verifier.js
+++ XRCobranza/src/lib/Guard/session_verifier.js
@@ -8,26 +8,18 @@ const keys = require(path.resolve('src/lib/guard', 'keys'));
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-async function session_verifier(abono) {
+async function session_verifier(_session) {
     var _global = [];
     try {
-        _global = cipher.decode(keys.security.client_password, req.body._global.data);
-        const login_sesion = await pool.query('CALL LOGIN_SESION(?)', [_global.hash]);
+        _global = cipher.decode(keys.security.client_password, _session);
+        var login_sesion = await pool.query('CALL LOGIN_SESION(?)', [_global.hash]);
         if (JSON.stringify(login_sesion) == '[]' || JSON.stringify(login_sesion) == "" || JSON.stringify(login_sesion) == null) {
-            res.status(400).send({
-                response: false,
-                session: false,
-                _global: null
-            });
+           return [];
         }
+        return _global;
     } catch (error) {
-        res.status(400).send({
-            response: false,
-            session: false,
-            _global: null,
-            error: error
-        });
         console.log(error);
+        return [];
     }
 }
 
