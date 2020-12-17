bed1ebbc54277ed0b1664914ef4a55692bdfefc9
xcoru <pcd510@hotmail.com>
2020-12-17 11:38:27 -0600

    Update util.js

--- XRCobranza/BackEnd/src/lib/util.js
+++ XRCobranza/BackEnd/src/lib/util.js
@@ -1,13 +1,16 @@
 const moment = require('moment');
 
+// FECHA Y HORA
 function getDateTime() {
     return moment().format('YYYY-MM-DD hh:mm:ss');
 }
 
+// FECHA GENERAL
 function getShortDate(){
     return moment().format('YYYY-MM-DD');
 }
 
+// FECHA MEXICO
 function getShortDateMX(){
     return moment().format('DD-MM-YYYY');
 }
